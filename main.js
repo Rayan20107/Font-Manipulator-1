a2be7c2ccbadc7bdee68a3b8e52041f4217e9c7b
Rayan20107 <75358640+Rayan20107@users.noreply.github.com>
2021-08-21 15:49:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+randomnumberred=Math.floor(Math.random()*255);
+
+randomnumberblue=Math.floor(Math.random()*255);
+
+randomnumbergreen=Math.floor(Math.random()*255);
+
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(600, 500);
+    Mycanvas=createCanvas(600, 400);
+    Mycanvas.position(650, 100);
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', results);
+}
+
+function modelLoaded()
+{
+    console.log("Model has been loaded");
+}
+
+function results(res)
+{
+    if (res.length > 0) {
+        console.log(res);
+    }
+}
+
+function draw()
+{
+    background(randomnumberred, randomnumbergreen, randomnumberblue);
+}
+
+function preload()
+{
+ 
+}
